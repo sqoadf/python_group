c42cb21a6dc81c1ffbb5267de189349839c595e4
sqoadf <timatrapeznikov11@gmail.com>
2022-09-29 19:09:35 +0300

    урок 4

---
+++ привет.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+bgcolor("black")
+color("red")
+begin_fill()
+penzize(3)
+left(50)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(133)
+end_fill()
+mainloop()
+left(90)
